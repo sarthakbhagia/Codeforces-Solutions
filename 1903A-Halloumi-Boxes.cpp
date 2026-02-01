721740b086d343b7da37b341a799587b20dc6045
Sarthak Bhagia <sarthakbhagia1@gmail.com>
2026-02-01 17:15:32 +0530

    Update 1903A-Halloumi-Boxes.cpp

--- 1903A-Halloumi-Boxes.cpp
+++ 1903A-Halloumi-Boxes.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+#include<iostream>
 #include<algorithm>
 using namespace std;
 int main(){
+    ios::sync_with_stdio(false); cin.tie(0);
     int t, n, k;
     cin >> t;
     int arr[10000];
